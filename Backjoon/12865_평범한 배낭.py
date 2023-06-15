3ea44c6ca38eb7e1eaf3d404cb9c5f9fa0698e0b
GT0122 <kmd1801@gmail.com>
2023-06-15 23:07:59 +0900

    [Feat] 230615 commit
    add 11382, 25314
    edit 12865

--- Backjoon/12865_평범한 배낭.py
+++ Backjoon/12865_평범한 배낭.py
@@ -3,41 +3,27 @@ n, k = map(int, input().split())
 items = []
 for i in range(n) :
     items.append([int(j) for j in input().split()])
-items.sort(key = lambda x : (x[1] / x[0], x[0]), reverse=True)
 
-q = []
-weight = 0
-for item in items :
-    if item[0] > k :
-        continue
-    
-    if weight + item[0] <= k :
-        q.append(item)
-        weight += item[0]
-    else :
-        del_list = []
-        cor_v = 0
-        cor_w = 0
-        while True :
-            if weight + (item[0] - cor_w) <= k :
-                break
-            w, v = q.pop(0)
-            del_list.append([w, v])
-            cor_v += v
-            cor_w += w
-        if cor_v < item[1] :
+max_value = 0
+while k > 0 :
+    pot = 0
+    index = 0
+    weight = k
+    value = 0
+    for i, (w, v) in enumerate(items) :
+        temp = v * (w / k)
+        if temp > pot :
+            pot = temp
+            weight = w
+            value = v
+            index = i
+        elif temp == pot and w < weight :
+            weight = w
+            value = v
+            index = i
 
-            q.append(item)
-            weight += item[0] - cor_w
-        elif cor_v == item[1] :
-            if cor_w >= item[0] :
-                q.append(item)
-                weight += item[0] - cor_w
-            else :
-                for w, v in del_list :
-                    q.append([w, v])
-        else :
-            for w, v in del_list :
-                q.append([w, v])
-print(q)
-print(sum([v for w, v in q]))
+    items.pop(index)
+    k -= weight
+    max_value += value
+    print(max_value, value, pot)
+print(max_value)
