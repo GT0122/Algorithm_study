e5c68cae1ee05d687b49eb80b48988d9ae7bc9f7
GT0122 <kmd1801@gmail.com>
2023-06-22 23:49:32 +0900

    [Feat] 230622 commit
    Edit 14501

--- Backjoon/14501_퇴사.py
+++ Backjoon/14501_퇴사.py
@@ -10,31 +10,14 @@ for i in range(n) :
     time.append(t)
     profit.append(p)
 
-day = n-1
-profits = []
-while day >= 0 :
-    today_t, today_p = time[day], profit[day]
-    profits.append(day)
-    if today_t > 1 :
-        temp_profits = set(profits[-today_t:-1])
-        loss = 0
-        for temp in temp_profits :
-            if temp != -1 :
-                loss += profit[temp]
-        if today_p > loss :
-            for i in range(1, today_t) :
-                profits[-(i+1)] = day
-            for temp in temp_profits :
-                if temp != -1 :
-                    while temp in profits and profits.index(temp) != -1 :
-                        profits[profits.index(temp)] = -1
-        else :
-            profits[-1] = -1
-    day -= 1
-    
-max_profit = 0
-for i in set(profits) :
-    if i != -1 :
-        max_profit += profit[i]
+profits = [0 for _ in range(n+1)]
+for i in range(n) :
+    t, p = time[i], profit[i]
+    # 각각의 날자에 최대값으로 갱신
+    # 일을 한다면 받을 수 있는 수익
+    profits[i + t] = max(profits[i + t], profits[i] + p)
+    # 일을 하지 않는 경우의 수익
+    profits[i + 1] = max(profits[i + 1], profits[i])
+    print(profits)
 
-print(max_profit)
+print(profits[n])
