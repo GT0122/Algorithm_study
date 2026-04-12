af6900d037e32f873dbe4c4b36a5e05e565c4dc8
GT0122 <kmd1801@gmail.com>
2026-04-12 23:26:16 +0900

    [Feat]11404_cpp_Baek

---
+++ Baekjoon/C++/acsBaek/11404_플로이드.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n, m, s, e, c;
+
+	cin >> n;
+	cin >> m;
+	vector<vector<int>> arr(n, vector<int>(n, 100001));
+
+	for (int i = 0; i < n; i++) arr[i][i] = 0;
+	
+	for (int i = 0; i < m; i++) {
+		cin >> s;
+		cin >> e;
+		cin >> c;
+
+		if(arr[s - 1][e - 1] > c) arr[s - 1][e - 1] = c;
+	}
+
+	for (int k = 0; k < n; k++) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (arr[i][k] * arr[k][j] > 0) {
+					if (arr[i][j] > arr[i][k] + arr[k][j]) arr[i][j] = arr[i][k] + arr[k][j];
+				}
+			}
+		}
+	}
+	
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (arr[i][j] == 100001) cout << 0 << ' ';
+			else cout << arr[i][j] << ' ';
+		}
+		cout << '\n';
+	}
+
+	return 0;
+}
