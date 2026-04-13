47eb6aafb0003a04e9125f1a71c42d16bf60de80
GT0122 <kmd1801@gmail.com>
2026-04-13 23:47:00 +0900

    [Feat]1987_cpp_Baek

---
+++ Baekjoon/C++/acsBaek/1987_알파벳.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int result = 0;
+
+void search(char arrC[][20], int* alpha, int n, int r, int c, int R, int C) {
+	result = max(result, n);
+	if (result >= 26) return;
+
+	alpha[arrC[r][c] - 65] |= 1;
+
+	if (r - 1 >= 0 and !(alpha[arrC[r - 1][c] - 65] & 1)) {
+		search(arrC, alpha, n + 1, r - 1, c, R, C);
+	}
+	if (r + 1 < R and !(alpha[arrC[r + 1][c] - 65] & 1)) {
+		search(arrC, alpha, n + 1, r + 1, c, R, C);
+	}
+	if (c - 1 >= 0 and !(alpha[arrC[r][c - 1] - 65] & 1)) {
+		search(arrC, alpha, n + 1, r, c - 1, R, C);
+	}
+	if (c + 1 < C and !(alpha[arrC[r][c + 1] - 65] & 1)) {
+		search(arrC, alpha, n + 1, r, c + 1, R, C);
+	}
+
+	alpha[arrC[r][c] - 65] &= 0;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	char arrC[20][20]{};
+	int alpha[26]{ 0, };
+
+	int R, C;
+	cin >> R >> C;
+
+	for (int i = 0; i < R; i++) {
+		for (int j = 0; j < C; j++) {
+			cin >> arrC[i][j];
+		}
+	}
+
+	search(arrC, alpha, 1, 0, 0, R, C);
+
+	cout << result;
+
+	return 0;
+}
